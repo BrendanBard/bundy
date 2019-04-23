bfa1b97b13bdacce10d041155eb6f182ca5c3cdc
malbrightin <49653292+malbrightin@users.noreply.github.com>
2019-04-23 19:50:25 -0400

    Upload log via ftp

---
+++ upload.ps1
@@ -0,0 +1,22 @@
+# Config
+$Username = "genera70"
+$Password = "123Buttercup!"
+$LocalFile = "C:\Temp\test.txt"
+$RemoteFile = "ftp://generalkenobi.x10host.com/public_html/test.txt"
+ 
+# Create FTP Rquest Object
+$FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
+$FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
+$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+$FTPRequest.Credentials = new-object System.Net.NetworkCredential($Username, $Password)
+$FTPRequest.UseBinary = $true
+$FTPRequest.UsePassive = $true
+# Read the File for Upload
+$FileContent = gc -en byte $LocalFile
+$FTPRequest.ContentLength = $FileContent.Length
+# Get Stream Request by bytes
+$Run = $FTPRequest.GetRequestStream()
+$Run.Write($FileContent, 0, $FileContent.Length)
+# Cleanup
+$Run.Close()
+$Run.Dispose()
